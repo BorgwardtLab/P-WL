763defbaf889d1c0f1f234dcb7a9b5d9e7cf6474
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2018-10-23 11:12:06 +0200

    Skeleton for weight assigner class

---
+++ weight_assigner.py
@@ -0,0 +1,25 @@
+class WeightAssigner:
+    '''
+    Given a labelled graph, this class assigns weights based on
+    a similarity measure and an assignment strategy and returns
+    the weighted graph.
+    '''
+
+    def __init__(self, ignore_label_order=False, similarity='jaccard'):
+        self._ignore_label_order = ignore_label_order
+        self._similarity = similarity
+
+    def fit_transform(self, graph):
+        pass
+
+    def _jaccard(A, B):
+        '''
+        Computes the Jaccard index between two sets of labels A and B. The
+        measure is in the range of [0,1], where 0 indicates no overlap and
+        1 indicates a perfect overlap.
+        '''
+
+        A = set(A)
+        B = set(B)
+
+        return len(A.intersection(B)) / len(A.union(B))
