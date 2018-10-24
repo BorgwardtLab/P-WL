32d8b65449d3f6a8691719bae0a67c19db8ee7c7
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2018-10-24 16:49:47 +0200

    Added basic support for labels

--- main.py
+++ main.py
@@ -5,18 +5,29 @@
 
 
 import igraph as ig
+import numpy as np
 
 import argparse
+import collections
 import logging
 
 from topology import PersistenceDiagramCalculator
 from weight_assigner import WeightAssigner  # FIXME: put this in a different module
 from WL import WL
 
+def read_labels(filename):
+    labels = []
+    with open(filename) as f:
+        labels = f.readlines()
+        labels = [label.strip() for label in labels]
+
+    return labels
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('FILES', nargs='+', help='Input graphs (in some supported format)')
+    parser.add_argument('-l', '--labels', type=str, help='Labels file', required=True)
     parser.add_argument('-n', '--num-iterations', default=3, type=int, help='Number of Weisfeiler-Lehman iterations')
     parser.add_argument('-f', '--filtration', type=str, default='sublevel', help='Filtration type')
 
@@ -25,14 +36,19 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     graphs = [ig.read(filename) for filename in args.FILES]
+    labels = read_labels(args.labels)
 
-    logger.debug('Read {} graphs'.format(len(graphs)))
+    logger.debug('Read {} graphs and {} labels'.format(len(graphs), len(labels)))
+
+    assert len(graphs) == len(labels)
 
     wl = WL()
     wa = WeightAssigner()
     pdc = PersistenceDiagramCalculator()  # FIXME: need to add order/filtration
 
-    for graph in graphs:
+    total_persistence_per_label = collections.defaultdict(list)
+
+    for graph, label in zip(graphs, labels):
         wl.fit_transform(graph, args.num_iterations)
 
         # Stores the new multi-labels that occur in every iteration,
@@ -49,4 +65,10 @@ if __name__ == '__main__':
 
             total_persistence_values.append(persistence_diagram.total_persistence())
 
-        print(total_persistence_values)
+        total_persistence_per_label[label].append(total_persistence_values)
+
+    for label in sorted(total_persistence_per_label.keys()):
+        total_persistence = total_persistence_per_label[label]
+        total_persistence = np.array(total_persistence).ravel()
+        print('Label:', label)
+        print('  - Mean:', np.mean(total_persistence))
