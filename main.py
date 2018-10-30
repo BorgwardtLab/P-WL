2ea7d63e321f4137799bb36c113b4c91742b01a2
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2018-10-30 15:09:40 +0100

    Removed obsolete training/feature generation code

--- main.py
+++ main.py
@@ -37,36 +37,8 @@ def main(args, logger):
 
     assert len(graphs) == len(labels)
 
-    #wl = WeisfeilerLehman()
-    #wa = WeightAssigner(metric='minkowski', p=2.0)
-    #pfg = PersistenceFeaturesGenerator(use_infinity_norm=False,
-    #                                   use_total_persistence=False,
-    #                                   use_label_persistence=True,
-    #                                   use_cycle_persistence=True,
-    #                                   p=2.0)
-
     y = np.array(labels)
-    X = PersistentWeisfeilerLehman(num_iterations=args.num_iterations).fit_transform(graphs, y) #np.zeros((len(graphs), args.num_iterations + 1))
-
-    #label_dicts = wl.fit_transform(graphs, args.num_iterations)
-
-    #X_per_iteration = []
-    #for iteration in tqdm(sorted(label_dicts.keys())):
-
-    #    weighted_graphs = [graph.copy() for graph in graphs]
-
-    #    for graph_index in sorted(label_dicts[iteration].keys()):
-    #        labels_raw, labels_compressed = label_dicts[iteration][graph_index]
-
-    #        weighted_graphs[graph_index].vs['label'] = labels_raw
-    #        weighted_graphs[graph_index].vs['compressed_label'] = labels_compressed
-
-    #        # TODO: rewrite weight assigner class to support lists
-    #        weighted_graphs[graph_index] = wa.fit_transform(weighted_graphs[graph_index])
-
-    #    X_per_iteration.append(pfg.fit_transform(weighted_graphs))
-
-    #X = np.concatenate(X_per_iteration, axis=1)
+    X = PersistentWeisfeilerLehman(num_iterations=args.num_iterations).fit_transform(graphs, y)
 
     np.random.seed(42)
     cv = StratifiedKFold(n_splits=10, shuffle=True)
