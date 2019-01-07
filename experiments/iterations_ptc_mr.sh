37c97756462a4ab62f1b4ba1791c70ba59e51dc8
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2019-01-07 17:36:16 +0100

    Fixed sequence number

--- experiments/iterations_ptc_mr.sh
+++ experiments/iterations_ptc_mr.sh
@@ -8,7 +8,7 @@ SCRIPT=../main.py
 
 echo "h acc_topological sdev_topological acc_original sdev_original"
 
-for h in `seq 0 0`; do
+for h in `seq 0 10`; do
   ACCURACY_TOP=`$SCRIPT    -n $h $GRAPHS -l $LABELS 2>&1 | grep Accuracy  | cut -f 2,4 -d " "`
   ACCURACY_ORG=`$SCRIPT -s -n $h $GRAPHS -l $LABELS 2>&1 | grep Accuracy  | cut -f 2,4 -d " "`
   echo $h $ACCURACY_TOP $ACCURACY_ORG
