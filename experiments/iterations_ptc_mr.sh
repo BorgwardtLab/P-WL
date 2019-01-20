0f9602d96db67b62cbb65929fb17b145a969cf61
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2019-01-20 20:17:21 +0100

    Added best power parameter for experiments

--- experiments/iterations_ptc_mr.sh
+++ experiments/iterations_ptc_mr.sh
@@ -9,8 +9,8 @@ SCRIPT=../main.py
 echo "h acc_topological sdev_topological acc_original sdev_original"
 
 for h in `seq 0 10`; do
-  ACCURACY_TOP=`$SCRIPT    -n $h $GRAPHS -l $LABELS 2>&1 | grep Accuracy  | cut -f 2,4 -d " "`
-  ACCURACY_ORG=`$SCRIPT -u -n $h $GRAPHS -l $LABELS 2>&1 | grep Accuracy  | cut -f 2,4 -d " "`
+  ACCURACY_TOP=`$SCRIPT -p 1 -n $h $GRAPHS -l $LABELS 2>&1 | grep Accuracy  | cut -f 2,4 -d " "`
+  ACCURACY_ORG=`$SCRIPT   -u -n $h $GRAPHS -l $LABELS 2>&1 | grep Accuracy  | cut -f 2,4 -d " "`
   echo $h $ACCURACY_TOP $ACCURACY_ORG
 done
 
