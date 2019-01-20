e31a859d3e4c7b79014bfab6d9b19018d5acfcb5
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2019-01-20 19:35:30 +0100

    Adjusted ablation experiment

--- experiments/cycles_ptc_mr.sh
+++ experiments/cycles_ptc_mr.sh
@@ -10,7 +10,7 @@ echo "h acc_topological_c sdev_topological_c acc_original sdev_original"
 
 for h in `seq 0 10`; do
   ACCURACY_TOP=`$SCRIPT -c -n $h $GRAPHS -l $LABELS 2>&1 | grep Accuracy  | cut -f 2,4 -d " "`
-  ACCURACY_ORG=`$SCRIPT -s -n $h $GRAPHS -l $LABELS 2>&1 | grep Accuracy  | cut -f 2,4 -d " "`
+  ACCURACY_ORG=`$SCRIPT -u -n $h $GRAPHS -l $LABELS 2>&1 | grep Accuracy  | cut -f 2,4 -d " "`
   echo $h $ACCURACY_TOP $ACCURACY_ORG
 done
 
--- experiments/iterations_ptc_mr.sh
+++ experiments/iterations_ptc_mr.sh
@@ -10,7 +10,7 @@ echo "h acc_topological sdev_topological acc_original sdev_original"
 
 for h in `seq 0 10`; do
   ACCURACY_TOP=`$SCRIPT    -n $h $GRAPHS -l $LABELS 2>&1 | grep Accuracy  | cut -f 2,4 -d " "`
-  ACCURACY_ORG=`$SCRIPT -s -n $h $GRAPHS -l $LABELS 2>&1 | grep Accuracy  | cut -f 2,4 -d " "`
+  ACCURACY_ORG=`$SCRIPT -u -n $h $GRAPHS -l $LABELS 2>&1 | grep Accuracy  | cut -f 2,4 -d " "`
   echo $h $ACCURACY_TOP $ACCURACY_ORG
 done
 
