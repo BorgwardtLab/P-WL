7aa81438b73718f9a320bc5bfbab74235274ecaa
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2019-03-11 14:10:24 +0100

    Fixed coding style issues for main file
    Also added a fixed `random_state` declaration for the classifier in
    order to make the results reproducible.

--- main.py
+++ main.py
@@ -47,7 +47,9 @@ def main(args, logger):
         if 'label' not in graph.vs.attributes():
             graph.vs['label'] = [0] * len(graph.vs)
 
-    logger.info('Read {} graphs and {} labels'.format(len(graphs), len(labels)))
+    logger.info(
+        'Read {} graphs and {} labels'.format(len(graphs), len(labels))
+    )
 
     assert len(graphs) == len(labels)
 
@@ -67,6 +69,8 @@ def main(args, logger):
     if args.use_cycle_persistence:
         logger.info('Using cycle persistence')
 
+    # Ensures that labels are encoded correctly, regardless of whether
+    # they are numerical or not.
     y = LabelEncoder().fit_transform(labels)
 
     # This ignores *all* other feature generation methods and falls back
@@ -83,7 +87,9 @@ def main(args, logger):
             pwl.transform(graphs, args.num_iterations)
 
     logger.info('Finished persistent Weisfeiler-Lehman transformation')
-    logger.info('Obtained ({} x {}) feature matrix'.format(X.shape[0], X.shape[1]))
+    logger.info(
+        'Obtained ({} x {}) feature matrix'.format(X.shape[0], X.shape[1])
+    )
 
     np.random.seed(42)
     cv = StratifiedKFold(n_splits=10, shuffle=True)
@@ -98,7 +104,8 @@ def main(args, logger):
         for train_index, test_index in cv.split(X, y):
             rf_clf = RandomForestClassifier(
                 n_estimators=50,
-                class_weight='balanced' if args.balanced else None
+                class_weight='balanced' if args.balanced else None,
+                random_state=42
             )
 
             if args.grid_search:
@@ -110,8 +117,11 @@ def main(args, logger):
                 )
 
                 grid_params = {
-                    'fs__num_iterations': np.arange(0, args.num_iterations + 1),
-                    'clf__n_estimators': [10, 20, 50, 100, 150, 200],
+                    'fs__num_iterations':
+                        np.arange(0, args.num_iterations + 1),
+
+                    'clf__n_estimators':
+                        [10, 20, 50, 100, 150, 200],
                 }
 
                 clf = GridSearchCV(
@@ -145,31 +155,83 @@ def main(args, logger):
             logger.debug('Best classifier for this fold: {}'.format(clf))
 
             if args.grid_search:
-                logger.debug('Best parameters for this fold: {}'.format(clf.best_params_))
+                logger.debug(
+                    'Best parameters for this fold: {}'.format(
+                        clf.best_params_)
+                    )
             else:
-                logger.debug('Best parameters for this fold: {}'.format(clf.get_params()))
+                logger.debug(
+                    'Best parameters for this fold: {}'.format(
+                        clf.get_params())
+                )
 
         mean_accuracies.append(np.mean(accuracy_scores))
-        logger.info('  - Mean 10-fold accuracy: {:2.2f} [running mean over all folds: {:2.2f}]'.format(mean_accuracies[-1] * 100, np.mean(mean_accuracies) * 100))
+        logger.info(
+            '''  - Mean 10-fold accuracy: {:2.2f} [running mean over all
+            folds: {:2.2f}]'''.format(
+                mean_accuracies[-1] * 100, np.mean(mean_accuracies) * 100)
+        )
 
-    logger.info('Accuracy: {:2.2f} +- {:2.2f}'.format(np.mean(mean_accuracies) * 100, np.std(mean_accuracies) * 100))
+    logger.info(
+        'Accuracy: {:2.2f} +- {:2.2f}'.format(
+            np.mean(mean_accuracies) * 100, np.std(mean_accuracies) * 100)
+    )
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('FILES', nargs='+', help='Input graphs (in some supported format)')
-    parser.add_argument('-b', '--balanced', action='store_true', help='Make random forest classifier balanced')
+    parser.add_argument(
+        'FILES', nargs='+', help='Input graphs (in some supported format)'
+    )
+
+    # Controls behaviour of the classifier (will be treated as
+    # a hyperparameter in the respective grid search scripts)
+    parser.add_argument(
+        '-b', '--balanced',
+        action='store_true', help='Make random forest classifier balanced'
+    )
+
     parser.add_argument('-d', '--dataset', help='Name of data set')
-    parser.add_argument('-l', '--labels', type=str, help='Labels file', required=True)
-    parser.add_argument('-n', '--num-iterations', default=3, type=int, help='Number of Weisfeiler-Lehman iterations')
-    parser.add_argument('-f', '--filtration', type=str, default='sublevel', help='Filtration type')
-    parser.add_argument('-g', '--grid-search', action='store_true', default=False, help='Whether to do hyperparameter grid search')
-    parser.add_argument('-c', '--use-cycle-persistence', action='store_true', default=False, help='Indicates whether cycle persistence should be calculated or not')
-    parser.add_argument('-o', '--use-original-features', action='store_true', default=False, help='Indicates that original features should be used as well')
+    parser.add_argument(
+        '-l', '--labels', type=str, help='Labels file', required=True
+    )
+
+    parser.add_argument(
+        '-n', '--num-iterations', default=3,
+        type=int, help='Number of Weisfeiler-Lehman iterations'
+    )
+
+    # TODO: unused
+    parser.add_argument(
+        '-f', '--filtration', type=str, default='sublevel',
+        help='Filtration type'
+    )
+
+    parser.add_argument(
+        '-g', '--grid-search', action='store_true',
+        default=False, help='Whether to do hyperparameter grid search'
+    )
+
+    parser.add_argument(
+        '-c', '--use-cycle-persistence', action='store_true', default=False,
+        help='Indicates whether cycle persistence should be calculated or not'
+    )
+
+    parser.add_argument(
+        '-o', '--use-original-features', action='store_true', default=False,
+        help='Indicates that original features should be used as well'
+    )
+
     # TODO: this flag is somewhat redundant given the flag above; need
     # to ensure that it is seen as an 'override', i.e. if this is set,
     # *no* other ways of calculating features can be used.
-    parser.add_argument('-s', '--use-subtree-features', action='store_true', default=False, help='Use Weisfeiler--Lehman subtree kernel instead of topological features')
+    parser.add_argument(
+        '-s', '--use-subtree-features', action='store_true', default=False,
+        help='''
+            Use Weisfeiler--Lehman subtree kernel instead of topological
+            features
+        '''
+    )
 
     ####################################################################
     # Metric selection options
