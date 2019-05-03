df9f380a7ac795751a002c55755cff7985ef03e7
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2019-05-03 17:25:59 +0200

    Extended comparison script to use smoothed distance

--- experiments/cycles_ptc_mr.sh
+++ experiments/cycles_ptc_mr.sh
@@ -4,13 +4,14 @@
 
 GRAPHS=../data/PTC_MR/*.gml
 LABELS=../data/PTC_MR/Labels.txt
-SCRIPT=../main.py
+SCRIPT=../src/main.py
 
 echo "h acc_topological_c sdev_topological_c acc_original sdev_original"
 
 for h in `seq 0 10`; do
   ACCURACY_TOP=`$SCRIPT -p 1 -c -n $h $GRAPHS -l $LABELS 2>&1 | grep Accuracy  | cut -f 2,4 -d " "`
   ACCURACY_ORG=`$SCRIPT      -u -n $h $GRAPHS -l $LABELS 2>&1 | grep Accuracy  | cut -f 2,4 -d " "`
-  echo $h $ACCURACY_TOP $ACCURACY_ORG
+  ACCURACY_SMOOTH=`$SCRIPT      -S -n $h $GRAPHS -l $LABELS 2>&1 | grep Accuracy  | cut -f 2,4 -d " "`
+  echo $h $ACCURACY_TOP $ACCURACY_ORG $ACCURACY_SMOOTH
 done
 
